20fed745e10484c2ac858aa8788f838a41772971
anoopksanoop <142888149+anoopksanoop@users.noreply.github.com>
2023-12-21 23:38:44 +0530

    first commit

---
+++ src/Body.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Body.css"
+const Body = () => {
+  return (
+    <div>
+               
+    <div className='triangle'>
+    
+        <div className='imgLogo'>
+        <img   src='https://media.licdn.com/dms/image/D560BAQFPvoYeqPTghg/company-logo_200_200/0/1683376259653?e=2147483647&v=beta&t=mas4QddQbvf9JJQsdVFQWf0OnHOp-B5sxgZQEFu-ZfE' alt=''/>
+        {/* <img className='image'  src='https://media.licdn.com/dms/image/D560BAQFPvoYeqPTghg/company-logo_200_200/0/1683376259653?e=2147483647&v=beta&t=mas4QddQbvf9JJQsdVFQWf0OnHOp-B5sxgZQEFu-ZfE' alt=''/> */}
+        </div>
+    </div>
+ 
+    </div>
+  )
+}
+
+export default Body
